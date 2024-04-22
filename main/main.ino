c3b3d2574adfb0fafd4d67c6b3cb6493979d1187
Wesley B. Johnson <wesleyjohnson773@gmail.com>
2024-04-22 12:35:04 -0600

    Set up main function

--- main/main.ino
+++ main/main.ino
@@ -3,7 +3,22 @@
 
 void loop() {
   Board b;
-  b.setBoard();
-  b.playGame();
+  bool newgame = true;
+  char buffer[1];
+
+  while(newgame){
+    b.setBoard();
+
+    while(b.playGame()){
+      Serial.println("New game? y for yes, anything else for no");
+      Serial.readBytes(buffer, 1);
+
+      if(buffer[0] != 'y'){
+        newgame = false;
+      }
+
+    }
+  }
+  
   delay(5000);
 }
